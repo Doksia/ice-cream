b0cb260dc6871e863a24437ad79077d67b7635ce
Miguel Romero <miguel.insti2@gmail.com>
2026-08-25 20:26:48 +0100

    solved update

--- src/pages/IceCreamDetailsPage.jsx
+++ src/pages/IceCreamDetailsPage.jsx
@@ -15,7 +15,7 @@ function IceCreamDetailsPage() {
 
   useEffect(() => {
     axios
-      .get(`${API_URL}/iceCreamId`)
+      .get(`${API_URL}/${iceCreamId}`)
       .then((response) => setIceCream(response.data))
       .catch(() => {
         alert("Ice cream not found");
