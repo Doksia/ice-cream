411347112ba72449a496f1eebfa07876d2d5a4e1
Moises Pastran <moilqp@gmail.com>
2026-08-28 10:29:59 +0200

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import Navbar from "./Navbar";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
   const logo = "https://cdn-icons-png.flaticon.com/512/3157/3157356.png";
 
   return (
@@ -10,7 +13,22 @@ function Header() {
         <img src={logo} alt="Logo" />
       </Link>
 
-      <Navbar />
+      {/* Botón hamburguesa */}
+      <button className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </button>
+
+      {/* Navbar normal (desktop) */}
+      <div className="navbar-desktop">
+        <Navbar />
+      </div>
+
+      {/* Navbar móvil */}
+      {open && (
+        <div className="navbar-mobile">
+          <Navbar />
+        </div>
+      )}
     </header>
   );
 }
